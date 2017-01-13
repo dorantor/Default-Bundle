c0812ed20915e3cba8479239e861635c9fce0f3a
Dracony <draconyster@gmail.com>
2017-01-13 18:20:12 +0100

    added processor helpers

--- src/PHPixie/DefaultBundle/HTTP/Processor.php
+++ src/PHPixie/DefaultBundle/HTTP/Processor.php
@@ -5,6 +5,7 @@ namespace PHPixie\DefaultBundle\HTTP;
 use PHPixie\DefaultBundle\Builder;
 use PHPixie\BundleFramework\Components;
 use PHPixie\DefaultBundle\Processor\HTTP\Actions;
+use PHPixie\HTTP\Responses\Response;
 
 /**
  * Your base command class
@@ -22,5 +23,31 @@ abstract class Processor extends Actions
     protected function components()
     {
         return $this->builder->components();
+    }
+
+    /**
+     * @return \PHPixie\HTTP\Responses
+     */
+    protected function responses()
+    {
+        return $this->builder->components()->http()->responses();
+    }
+
+    /**
+     * @return \PHPixie\Framework\HTTP
+     */
+    protected function frameworkHttp()
+    {
+        return $this->builder->frameworkBuilder()->http();
+    }
+
+    /**
+     * @param $route
+     * @param array $parameters
+     * @return Response
+     */
+    protected function redirect($route, $parameters = array())
+    {
+        return $this->frameworkHttp()->redirectResponse($route, $parameters);
     }
 }
